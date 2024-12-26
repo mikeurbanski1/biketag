e1b16e896d27a89020d6ed3982d65e5590e8908c
Mike Urbanski <mikeurbanski1@gmail.com>
2024-12-25 23:02:33 -0600

    add static nav bar

---
+++ apps/client/src/components/common/navBar.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+
+import { GameDto, UserDto } from '@biketag/models';
+
+interface NavBarProps {
+    user?: UserDto;
+    game?: GameDto;
+    handleLogout: () => void;
+}
+
+interface UserMenuProps {
+    user: UserDto;
+    handleLogout: () => void;
+}
+
+const UserMenu: React.FC<UserMenuProps> = ({ user, handleLogout }) => {
+    const userName = user.name;
+    const userNameClassName = 'clickable-text clickable-nav-item dropdown-header user-dropdown-header';
+    return (
+        <span className={userNameClassName}>
+            {userName.charAt(0)}
+            <div className="dropdown-content user-dropdown-content">
+                <span className="clickable-text" onClick={handleLogout}>
+                    Log out
+                </span>
+            </div>
+        </span>
+    );
+};
+
+const MainMenu: React.FC = () => {
+    return (
+        <span className="clickable-text clickable-nav-item menu-dropdown-header dropdown-header">
+            🚲<div className="dropdown-content menu-dropdown-content">ABCDEFG</div>
+        </span>
+    );
+};
+
+const NavBar: React.FC<NavBarProps> = ({ user, game, handleLogout }) => {
+    return (
+        <div className="nav-bar">
+            <span className="bike-tag-title">Bike Tag!</span>
+            {user ? <UserMenu user={user} handleLogout={handleLogout} /> : <span></span>}
+            <MainMenu />
+        </div>
+    );
+};
+
+export default NavBar;
