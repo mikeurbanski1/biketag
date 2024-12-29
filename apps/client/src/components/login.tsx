c1659606db9b64b1fb1f020f885ccd188eb8d5bb
Mike Urbanski <mikeurbanski1@gmail.com>
2024-12-29 14:18:12 -0600

    login functional component

--- apps/client/src/components/login.tsx
+++ apps/client/src/components/login.tsx
@@ -1,96 +1,138 @@
-import React, { ChangeEvent, ReactNode } from 'react';
+import React, { ChangeEvent } from 'react';
 
 import { UserDto } from '@biketag/models';
 
 import { ApiManager } from '../api';
 
-interface LoginState {
-    name: string;
-    user?: UserDto;
-    canLogin: boolean;
-    canSignup: boolean;
-    errorMessage?: string;
-}
-
 interface LoginProps {
     setUser: ({ name, id }: { name: string; id: string }) => void;
 }
 
-export class Login extends React.Component<LoginProps, LoginState> {
-    constructor(props: LoginProps) {
-        super(props);
-        this.state = {
-            name: '',
-            canLogin: false,
-            canSignup: false,
-        };
-        // this.handleNameChange = this.handleNameChange.bind(this);
-        // this.handleIdChange = this.handleIdChange.bind(this);
-        // this.login = this.login.bind(this);
-        // this.signUp = this.signUp.bind(this);
-    }
+export const Login: React.FC<LoginProps> = ({ setUser }: LoginProps) => {
+    const [name, setName] = React.useState('');
+    const [canLogin, setCanLogin] = React.useState(false);
+    const [canSignup, setCanSignup] = React.useState(false);
+    const [errorMessage, setErrorMessage] = React.useState<string | undefined>(undefined);
 
-    private handleNameChange(event: ChangeEvent<HTMLInputElement>) {
-        const newState: Partial<LoginState> = {
-            name: event.target.value,
-        };
-        newState.canLogin = this.canLogin(newState);
-        newState.canSignup = this.canSignup(newState);
-        this.setState(newState as LoginState);
-    }
+    const handleNameChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setName(event.target.value);
+        setCanLogin(event.target.value !== '');
+        setCanSignup(event.target.value !== '');
+    };
 
-    private canLogin(newState: Partial<LoginState>) {
-        // const merged = Object.assign({}, this.state, newState);
-        return newState.name !== '';
-    }
-
-    private canSignup(newState: Partial<LoginState>) {
-        // const merged = Object.assign({}, this.state, newState);
-        return newState.name !== '';
-    }
-
-    async login() {
+    const login = async () => {
         try {
-            const { name } = this.state;
             const { id } = await ApiManager.userApi.login({ name });
-            this.props.setUser({ name, id });
+            setUser({ name, id });
         } catch (err) {
             if (err instanceof Error) {
-                this.setState({
-                    errorMessage: err.message,
-                });
+                setErrorMessage(err.message);
             }
         }
-    }
+    };
 
-    async signUp() {
+    const signUp = async () => {
         try {
-            const { name } = this.state;
             const { id } = await ApiManager.userApi.signup({ name });
-            this.props.setUser({ name, id });
+            setUser({ name, id });
         } catch (err) {
             if (err instanceof Error) {
-                this.setState({
-                    errorMessage: err.message,
-                });
+                setErrorMessage(err.message);
             }
         }
-    }
+    };
 
-    public render(): ReactNode {
-        return (
-            <div className="flex-column moderate-gap">
-                <input className="login-text" placeholder="Name" type="text" onChange={(event) => this.handleNameChange(event)} value={this.state.name}></input>
-                <div className="button-pair">
-                    <button className="login-button" type="button" onClick={async () => await this.login()} disabled={!this.state.canSignup}>
-                        Login
-                    </button>
-                    <button className="login-button" type="button" onClick={async () => await this.signUp()} disabled={!this.state.canSignup}>
-                        Sign up
-                    </button>
-                </div>
-                <h3>{this.state.errorMessage || ''}</h3>
+    return (
+        <div className="flex-column moderate-gap">
+            <input className="login-text" placeholder="Name" type="text" onChange={(event) => handleNameChange(event)} value={name}></input>
+            <div className="button-pair">
+                <button className="login-button" type="button" onClick={async () => await login()} disabled={!canLogin}>
+                    Login
+                </button>
+                <button className="login-button" type="button" onClick={async () => await signUp()} disabled={!canSignup}>
+                    Sign up
+                </button>
             </div>
-        );
-    }
-}
+            <h3>{errorMessage || ''}</h3>
+        </div>
+    );
+};
+
+// export class Login2 extends React.Component<LoginProps, LoginState> {
+//     constructor(props: LoginProps) {
+//         super(props);
+//         this.state = {
+//             name: '',
+//             canLogin: false,
+//             canSignup: false,
+//         };
+//         // this.handleNameChange = this.handleNameChange.bind(this);
+//         // this.handleIdChange = this.handleIdChange.bind(this);
+//         // this.login = this.login.bind(this);
+//         // this.signUp = this.signUp.bind(this);
+//     }
+
+//     private handleNameChange(event: ChangeEvent<HTMLInputElement>) {
+//         const newState: Partial<LoginState> = {
+//             name: event.target.value,
+//         };
+//         newState.canLogin = this.canLogin(newState);
+//         newState.canSignup = this.canSignup(newState);
+//         this.setState(newState as LoginState);
+//     }
+
+//     private canLogin(newState: Partial<LoginState>) {
+//         // const merged = Object.assign({}, this.state, newState);
+//         return newState.name !== '';
+//     }
+
+//     private canSignup(newState: Partial<LoginState>) {
+//         // const merged = Object.assign({}, this.state, newState);
+//         return newState.name !== '';
+//     }
+
+//     async login() {
+//         try {
+//             const { name } = this.state;
+//             const { id } = await ApiManager.userApi.login({ name });
+//             this.props.setUser({ name, id });
+//         } catch (err) {
+//             if (err instanceof Error) {
+//                 this.setState({
+//                     errorMessage: err.message,
+//                 });
+//             }
+//         }
+//     }
+
+//     async signUp() {
+//         try {
+//             const { name } = this.state;
+//             const { id } = await ApiManager.userApi.signup({ name });
+//             this.props.setUser({ name, id });
+//         } catch (err) {
+//             if (err instanceof Error) {
+//                 this.setState({
+//                     errorMessage: err.message,
+//                 });
+//             }
+//         }
+//     }
+
+//     public render(): ReactNode {
+//         return (
+//             <div className="flex-column moderate-gap">
+//                 <input className="login-text" placeholder="Name" type="text" onChange={(event) => this.handleNameChange(event)} value={this.state.name}></input>
+//                 <div className="button-pair">
+//                     <button className="login-button" type="button" onClick={async () => await this.login()} disabled={!this.state.canSignup}>
+//                         Login
+//                     </button>
+//                     <button className="login-button" type="button" onClick={async () => await this.signUp()} disabled={!this.state.canSignup}>
+//                         Sign up
+//                     </button>
+//                 </div>
+//                 <h3>{this.state.errorMessage || ''}</h3>
+//             </div>
+//         );
+//     }
+// }
